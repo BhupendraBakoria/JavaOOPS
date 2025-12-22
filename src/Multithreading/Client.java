25694300e5a041fba4d11bae9fb978a7c79bd612
BhupendraBakoria <bhupendrabak13@gmail.com>
2025-12-22 11:56:46 +0530

    Updating the code

--- src/Multithreading/Client.java
+++ src/Multithreading/Client.java
@@ -1,5 +1,12 @@
 package Multithreading;
 
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
+
 public class Client {
+    public static void main(String[] args) throws Exception {
+        Fibonacci fibNumber = new Fibonacci(10);
+        System.out.println(fibNumber.call());
+    }
 
 }
